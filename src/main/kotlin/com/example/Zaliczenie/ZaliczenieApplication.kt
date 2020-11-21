61d5244d2e6c08558326486b4ee85d6a5a3b6d98
Krystian Winiarczyk <51743540+Krystian-Winiarczyk@users.noreply.github.com>
2020-11-21 10:19:24 +0100

    initial commit

---
+++ src/main/kotlin/com/example/Zaliczenie/ZaliczenieApplication.kt
@@ -0,0 +1,11 @@
+package com.example.Zaliczenie
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZaliczenieApplication
+
+fun main(args: Array<String>) {
+	runApplication<ZaliczenieApplication>(*args)
+}
